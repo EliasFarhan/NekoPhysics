e14d521095466da0f928b6e9b2fa106ba1b92010
Elias Farhan <elias.farhan@gmail.com>
2024-09-02 17:27:51 +0200

    Moving math func from fixed lut to own header

---
+++ include/math/func.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <cmath>
+
+
+namespace neko
+{
+
+template<typename T>
+constexpr T Abs(T value)
+{
+	if(value >= T{0.0f})
+		return value;
+	return -value;
+}
+
+template<typename T>
+constexpr T Clamp(T value, T minValue, T maxValue)
+{
+	if(value > maxValue)
+		return maxValue;
+	if(value < minValue)
+		return minValue;
+	return value;
+}
+
+template<typename T>
+constexpr T Max(T v1, T v2)
+{
+	return v1 > v2 ? v1 : v2;
+}
+
+template<typename T>
+constexpr T Min(T v1, T v2)
+{
+	return v1 < v2 ? v1 : v2;
+}
+
+template<typename T>
+constexpr T Sign(T v1)
+{
+	if(v1 < T{0.0f})
+	{
+		return T{-1.0f};
+	}
+	return T{1.0f};
+}
+
+template<typename T>
+constexpr T Lerp(T begin, T end, T t)
+{
+	return (end-begin)*t+begin;
+}
+
+template<typename T>
+T Sqrt(T value)
+{
+	return std::sqrt(value);
+}
+
+template<typename T>
+T Sin(T value)
+{
+	return std::sin(value);
+}
+
+template<typename T>
+T Cos(T value)
+{
+	return std::cos(value);
+}
+}
