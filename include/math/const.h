bfd77d43e59a46c3c489853740f2febcb3b07fdd
Elias Farhan <elias.farhan@gmail.com>
2024-09-21 21:05:31 -0600

    Correct warning on const

--- include/math/const.h
+++ include/math/const.h
@@ -11,13 +11,13 @@ using Scalar = Fixed16;
 template<typename T>
 constexpr T Pi()
 {
-    return 3.14159265358979323846264338327950288;
+    return static_cast<T>(3.14159265358979323846264338327950288);
 }
 
 template<typename T>
 constexpr T Exp()
 {
-	return 2.718281828459045;
+	return static_cast<T>(2.718281828459045);
 }
 
 template<>
