0c20b06da31d118c4645c29246e4f4d7cbc96357
Elias Farhan <elias.farhan@gmail.com>
2026-01-13 12:05:46 +0100

    Added option to sixit float in cmake or not

--- include/math/const.h
+++ include/math/const.h
@@ -1,23 +1,31 @@
 #pragma once
 
 #include "math/fixed.h"
+#ifdef NEKO_USE_SIXIT
 #include "math/sixit_float.h"
+#endif
 #include <cmath>
 #include <numbers>
 
 namespace neko
 {
 
+#ifdef NEKO_USE_SIXIT
 using Scalar = sixit::dmath::ieee_float_inline_asm;
+#else
+using Scalar = float;
+#endif
 
 template<typename T>
 constexpr T Pi()
 {
+#ifdef NEKO_USE_SIXIT
     if constexpr (std::is_same_v<T, sixit::dmath::ieee_float_inline_asm>)
     {
         return sixit::dmath::ieee_float_inline_asm(std::numbers::pi_v<float>);
     }
     else
+#endif
     {
         return std::numbers::pi_v<T>;
     }
@@ -26,11 +34,13 @@ constexpr T Pi()
 template<typename T>
 constexpr T Exp()
 {
+#ifdef NEKO_USE_SIXIT
     if constexpr (std::is_same_v<T, sixit::dmath::ieee_float_inline_asm>)
     {
         return sixit::dmath::ieee_float_inline_asm(std::numbers::e_v<float>);
     }
     else
+#endif
     {
         return std::numbers::e_v<T>;
     }
